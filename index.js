fe1736fe947363750b365fae03cdbd0d0941c0ce
Katherine Tsai <katherinetsai@DESKTOP-10SDEV1.localdomain>
2022-11-06 21:48:31 -0500

    commit tree 37af3a66855d13700dc6ad235683024853dcd110

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+const userData = {
+    name: "Steve",
+    email: "steve@steve.com",
+};
+
+const object = {
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    body: JSON.stringify(userData),
+}
+
+const submitData = () => {
+    return fetch("http://localhost:3000/users", object)
+        .then(response => response.json())
+        .then((data) => {
+            let p = document.createElement("p")
+            p.innerText = data.id
+            document.body.appendChild(p)
+        })
+        .catch((error) => {
+            alert("Bad things! Ragnarők!");
+            console.log(error.message);
+        })
+
+
+}
+
+submitData();
